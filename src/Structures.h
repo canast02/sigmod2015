c18d0a86c04d5de6c335f9f36b58e26ddeb0aa2f
costantinos <costa.costantinos@gmail.com>
2015-03-02 02:45:52 +0200

    Added headers

---
+++ src/Structures.h
@@ -0,0 +1,137 @@
+/*
+ * structures.h
+ *
+ *  Created on: Mar 2, 2015
+ *      Author: costantinos
+ */
+
+#ifndef STRUCTURES_H_
+#define STRUCTURES_H_
+
+#define OPERATORS 7
+//#define THREADS 4
+//#define DEBUG
+
+//--------------------------------------------------
+// Wire protocol messages
+//--------------------------------------------------
+struct MessageHead {
+	/// Message types
+	enum Type
+		: uint32_t {Done,
+		DefineSchema,
+		Transaction,
+		ValidationQueries,
+		Flush,
+		Forget
+	};
+	/// Total message length, excluding this head
+	uint32_t messageLen;
+	/// The message type
+	Type type;
+};
+//--------------------------------------------------
+struct DefineSchema {
+	/// Number of relations
+	uint32_t relationCount;
+	/// Column counts per relation, one count per relation. The first column is always the primary key
+	uint32_t columnCounts[];
+};
+//--------------------------------------------------
+struct Transaction {
+	/// The transaction id. Monotonic increasing
+	uint64_t transactionId;
+	/// The operation counts
+	uint32_t deleteCount, insertCount;
+	/// A sequence of transaction operations. Deletes first, total deleteCount+insertCount operations
+	char operations[];
+};
+//--------------------------------------------------
+struct TransactionOperationDelete {
+	/// The affected relation
+	uint32_t relationId;
+	/// The row count
+	uint32_t rowCount;
+	/// The deleted values, rowCount primary keyss
+	uint64_t keys[];
+};
+//--------------------------------------------------
+struct TransactionOperationInsert {
+	/// The affected relation
+	uint32_t relationId;
+	/// The row count
+	uint32_t rowCount;
+	/// The inserted values, rowCount*relation[relationId].columnCount values
+	uint64_t values[];
+};
+//--------------------------------------------------
+struct ValidationQueries {
+	/// The validation id. Monotonic increasing
+	uint64_t validationId;
+	/// The transaction range
+	uint64_t from, to;
+	/// The query count
+	uint32_t queryCount;
+	/// The queries
+	char queries[];
+};
+//--------------------------------------------------
+struct Query {
+	/// A column description
+	struct Column {
+		/// Support operations
+		enum Op
+			: uint32_t {Equal,
+			NotEqual,
+			Less,
+			LessOrEqual,
+			Greater,
+			GreaterOrEqual,
+			Invalid
+		};
+		/// The column id
+		uint32_t column;
+		/// The operations
+		Op op;
+		/// The constant
+		uint64_t value;
+	};
+	/// The relation
+	uint32_t relationId;
+	/// The number of bound columns
+	uint32_t columnCount;
+	/// The bindings
+	Column columns[];
+};
+
+
+inline static bool queryComparator(const Query* q1, const Query* q2) {
+	return (q1->columnCount < q2->columnCount);
+}
+inline static bool columnComparator(Query::Column& c1, Query::Column& c2) {
+	return (c1.op < c2.op);
+}
+
+inline static bool columnOpComparator(Query::Column& c1, Query::Column& c2) {
+	return (c1.column < c2.column);
+}
+
+//--------------------------------------------------
+struct Flush {
+	/// All validations to this id (including) must be answered
+	uint64_t validationId;
+};
+//--------------------------------------------------
+struct Forget {
+	/// Transactions older than that (including) will not be tested for
+	uint64_t transactionId;
+};
+#ifdef DEBUG
+int Equal = 0, NotEqual = 0, Less = 0, LessOrEqual = 0, Greater = 0,
+GreaterOrEqual = 0, Invalid = 0;
+#endif
+
+
+
+
+#endif /* STRUCTURES_H_ */
---
+++ src/Validation.h
@@ -0,0 +1,507 @@
+/*
+ * validation.h
+ *
+ *  Created on: Mar 2, 2015
+ *      Author: costantinos
+ */
+
+#ifndef VALIDATION_H_
+#define VALIDATION_H_
+
+#include <unordered_map>
+#include <algorithm>    // std::sort
+#include <vector>
+
+#include "Structures.h"
+
+inline static bool isQueryValid(Query* q) {
+	std::unordered_map<uint32_t, Query::Column *[OPERATORS]> opsMap;
+	std::sort(q->columns, q->columns + q->columnCount, columnOpComparator);
+
+	for (unsigned i = 0; i < q->columnCount; i++) {
+		auto curQueryOp = &(q->columns[i]);
+		//for each column check
+		if (opsMap.find(curQueryOp->column) == opsMap.end()) {
+			for (int i = 0; i < OPERATORS; ++i) {
+				opsMap[curQueryOp->column][i] = NULL;
+			}
+			opsMap[curQueryOp->column][curQueryOp->op] = curQueryOp;
+		} else {
+			//Get the column from the columns map
+			Query::Column ** opColumn = opsMap[curQueryOp->column];
+
+			//Fill the operation list
+			if (opColumn[curQueryOp->op] == NULL) {
+				//save
+				opColumn[curQueryOp->op] = curQueryOp;
+			}
+
+			//check the operations of a specific column
+			switch (curQueryOp->op) {
+			case Query::Column::Equal:
+#ifdef DEBUG
+				++Equal;
+#endif
+				//Check c0==<value1> && c0==<value2>, value1!=value2
+				if (opColumn[curQueryOp->op] == NULL) {
+					//save
+					opColumn[curQueryOp->op] = curQueryOp;
+				}
+				if (opColumn[Query::Column::Equal] != NULL
+						&& opColumn[Query::Column::Equal]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::Equal]->value
+							!= curQueryOp->value) {
+#ifdef DEBUG
+						cerr << "==" << endl;
+#endif
+						return false;
+					}
+				}
+				//Check c0!=<value1> && c0==<value2>, value1==value2
+				if (opColumn[Query::Column::NotEqual] != NULL
+						&& opColumn[Query::Column::NotEqual]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::NotEqual]->value
+							== curQueryOp->value) {
+#ifdef DEBUG
+						cerr << "!=" << endl;
+#endif
+						return false;
+					} else {
+						opColumn[Query::Column::NotEqual]->op =
+								Query::Column::Invalid;
+						opColumn[Query::Column::NotEqual] = NULL;
+					}
+				}
+				//Check c0<=<value1> && c0==<value2>, value1<value2
+				if (opColumn[Query::Column::LessOrEqual] != NULL
+						&& opColumn[Query::Column::LessOrEqual]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::LessOrEqual]->value
+							< curQueryOp->value) {
+#ifdef DEBUG
+						cerr << "<=" << endl;
+#endif
+						return false;
+					} else {
+						opColumn[Query::Column::LessOrEqual]->op =
+								Query::Column::Invalid;
+						opColumn[Query::Column::LessOrEqual] = NULL;
+					}
+				}
+				//Check c0>=<value1> && c0==<value2>, value1==value2
+				if (opColumn[Query::Column::GreaterOrEqual] != NULL
+						&& opColumn[Query::Column::GreaterOrEqual]->op
+								!= Query::Column::Invalid) {
+
+					if (opColumn[Query::Column::GreaterOrEqual]->value
+							> curQueryOp->value) {
+#ifdef DEBUG
+						cerr << ">=" << endl;
+#endif
+						return false;
+					} else {
+						opColumn[Query::Column::GreaterOrEqual]->op =
+								Query::Column::Invalid;
+						opColumn[Query::Column::GreaterOrEqual] = NULL;
+					}
+				}
+				//Check c0<<value1> && c0==<value2>, value1<=value2
+				if (opColumn[Query::Column::Less] != NULL
+						&& opColumn[Query::Column::Less]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::Less]->value
+							<= curQueryOp->value) {
+#ifdef DEBUG
+						cerr << "<" << endl;
+#endif
+						return false;
+					} else {
+						opColumn[Query::Column::Less]->op =
+								Query::Column::Invalid;
+						opColumn[Query::Column::Less] = NULL;
+					}
+				}
+				//Check c0><value1> && c0==<value2>, value1>=value2
+				if (opColumn[Query::Column::Greater] != NULL
+						&& opColumn[Query::Column::Greater]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::Greater]->value
+							>= curQueryOp->value) {
+#ifdef DEBUG
+						cerr << ">" << endl;
+#endif
+						return false;
+					} else {
+						opColumn[Query::Column::Greater]->op =
+								Query::Column::Invalid;
+						opColumn[Query::Column::Greater] = NULL;
+					}
+				}
+				break;
+////////////////////////////////////////////////////////////////////////////////////////
+				//Dont do anything
+			case Query::Column::NotEqual:
+#ifdef DEBUG
+				++NotEqual;
+#endif
+				break;
+////////////////////////////////////////////////////////////////////////////////////////
+			case Query::Column::Less:
+#ifdef DEBUG
+				++Less;
+#endif
+				//Check c0==value1> && c0<<value2>, value1>=value2
+				if (opColumn[Query::Column::Equal] != NULL
+						&& opColumn[Query::Column::Equal]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::Equal]->value
+							>= curQueryOp->value) {
+						return false;
+					}
+
+				}
+				//Check c0><value1> && c0<<value2>, value1>=value2
+				if (opColumn[Query::Column::Greater] != NULL
+						&& opColumn[Query::Column::Greater]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::Greater]->value
+							>= curQueryOp->value) {
+						return false;
+					}
+				}
+				//Check c0>=<value1> && c0<<value2>, value1>=value2
+				if (opColumn[Query::Column::GreaterOrEqual] != NULL
+						&& opColumn[Query::Column::GreaterOrEqual]->op
+								!= Query::Column::Invalid) {
+
+					if (opColumn[Query::Column::GreaterOrEqual]->value
+							>= curQueryOp->value) {
+						return false;
+					}
+				}
+
+				//Check c0!=<value1> && c0<<value2>, value1<=value2
+				if (opColumn[Query::Column::NotEqual] != NULL
+						&& opColumn[Query::Column::NotEqual]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::NotEqual]->value
+							> curQueryOp->value) {
+						//TODO: remove != from queries
+						opColumn[Query::Column::NotEqual]->op =
+								Query::Column::Invalid;
+
+					}
+				}
+
+				//Check c0<=<value1> && c0<<value2>
+				if (opColumn[Query::Column::LessOrEqual] != NULL
+						&& opColumn[Query::Column::LessOrEqual]->op
+								!= Query::Column::Invalid) {
+					//TODO: keep the smallest one from queries
+					if (opColumn[Query::Column::LessOrEqual]->value
+							>= curQueryOp->value) {
+						opColumn[Query::Column::LessOrEqual]->op =
+								Query::Column::Invalid;
+					} else {
+						curQueryOp->op = Query::Column::Invalid;
+						break;
+					}
+				}
+
+				//Check c0<<value1> && c0<<value2>
+				if (opColumn[Query::Column::Less] != NULL
+						&& opColumn[Query::Column::Less]->op
+								!= Query::Column::Invalid
+						&& opColumn[Query::Column::Less] != curQueryOp) {
+					//TODO: Keep the smallest
+					if (curQueryOp->value
+							<= opColumn[Query::Column::Less]->value) {
+						opColumn[Query::Column::Less]->op =
+								Query::Column::Invalid;
+						opColumn[Query::Column::Less] = curQueryOp;
+					} else {
+						curQueryOp->op = Query::Column::Invalid;
+						break;
+					}
+				}
+
+				break;
+//////////////////////////////////////////////////////////////////////////////////////////
+			case Query::Column::LessOrEqual:
+#ifdef DEBUG
+				++LessOrEqual;
+#endif
+				//Check c0==value1> && c0<=<value2>, value1>=value2
+				if (opColumn[Query::Column::Equal] != NULL
+						&& opColumn[Query::Column::Equal]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::Equal]->value
+							> curQueryOp->value) {
+						return false;
+					}
+
+				}
+
+				//Check c0>=<value1> && c0<=<value2>, value1>value2
+				if (opColumn[Query::Column::GreaterOrEqual] != NULL
+						&& opColumn[Query::Column::GreaterOrEqual]->op
+								!= Query::Column::Invalid) {
+
+					if (opColumn[Query::Column::GreaterOrEqual]->value
+							> curQueryOp->value) {
+						return false;
+					}
+					//TODO: if(value1 == value2) replace with equal
+					else if (opColumn[Query::Column::GreaterOrEqual]->value
+							== curQueryOp->value) {
+						opColumn[Query::Column::GreaterOrEqual]->op =
+								Query::Column::Invalid;
+						opColumn[Query::Column::GreaterOrEqual] = NULL;
+						curQueryOp->op = Query::Column::Equal;
+					}
+				}
+
+				//Check c0><value1> && c0<=<value2>, value1>=value2
+				if (opColumn[Query::Column::Greater] != NULL
+						&& opColumn[Query::Column::Greater]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::Greater]->value
+							> curQueryOp->value) {
+						return false;
+					}
+				}
+
+				//Check c0!=<value1> && c0<=<value2> value1<=value2
+				if (opColumn[Query::Column::NotEqual] != NULL
+						&& opColumn[Query::Column::NotEqual]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::NotEqual]->value
+							> curQueryOp->value) {
+						//TODO: remove != from queries
+						opColumn[Query::Column::NotEqual]->op =
+								Query::Column::Invalid;
+						opColumn[Query::Column::NotEqual] = NULL;
+
+					}
+				}
+
+				//Check c0<=<value1> && c0<=<value2>
+				if (opColumn[Query::Column::LessOrEqual] != NULL
+						&& opColumn[Query::Column::LessOrEqual]->op
+								!= Query::Column::Invalid
+						&& opColumn[Query::Column::LessOrEqual] != curQueryOp) {
+					//TODO: keep the smallest one from queries
+					if (opColumn[Query::Column::LessOrEqual]->value
+							>= curQueryOp->value) {
+						opColumn[Query::Column::LessOrEqual]->op =
+								Query::Column::Invalid;
+						opColumn[Query::Column::LessOrEqual] = curQueryOp;
+					} else {
+						curQueryOp->op = Query::Column::Invalid;
+						break;
+					}
+				}
+
+				//Check c0<<value1> && c0<=<value2>
+				if (opColumn[Query::Column::Less] != NULL
+						&& opColumn[Query::Column::Less]->op
+								!= Query::Column::Invalid) {
+					//TODO: Keep the smallest
+					if (opColumn[Query::Column::Less]->value
+							> curQueryOp->value) {
+						opColumn[Query::Column::Less]->op =
+								Query::Column::Invalid;
+						opColumn[Query::Column::Less] = NULL;
+					} else {
+						curQueryOp->op = Query::Column::Invalid;
+						break;
+					}
+				}
+
+				break;
+//				////////////////////////////////////////////////////////////////////////////////////////
+			case Query::Column::Greater:
+#ifdef DEBUG
+				++Greater;
+#endif
+				//Check c0==value1> && c0><value2>, value1>=value2
+				if (opColumn[Query::Column::Equal] != NULL
+						&& opColumn[Query::Column::Equal]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::Equal]->value
+							<= curQueryOp->value) {
+						return false;
+					}
+
+				}
+
+				//Check c0<=<value1> && c0><value2>
+				if (opColumn[Query::Column::LessOrEqual] != NULL
+						&& opColumn[Query::Column::LessOrEqual]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::LessOrEqual]->value
+							<= curQueryOp->value) {
+						return false;
+					}
+				}
+
+				//Check c0<<value1> && c0><value2>
+				if (opColumn[Query::Column::Less] != NULL
+						&& opColumn[Query::Column::Less]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::Less]->value
+							<= curQueryOp->value) {
+						return false;
+					}
+				}
+				//Check c0!=<value1> && c0><value2>, value1<=value2
+				if (opColumn[Query::Column::NotEqual] != NULL
+						&& opColumn[Query::Column::NotEqual]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::NotEqual]->value
+							<= curQueryOp->value) {
+						//TODO: remove != from queries
+						opColumn[Query::Column::NotEqual]->op =
+								Query::Column::Invalid;
+						opColumn[Query::Column::NotEqual] = NULL;
+
+					}
+				}
+
+				//Check c0>=<value1> && c0><value2>
+				if (opColumn[Query::Column::GreaterOrEqual] != NULL
+						&& opColumn[Query::Column::GreaterOrEqual]->op
+								!= Query::Column::Invalid) {
+					//TODO: Keep the max
+					if (opColumn[Query::Column::GreaterOrEqual]->value
+							> curQueryOp->value) {
+						curQueryOp->op = Query::Column::Invalid;
+						break;
+					} else {
+						opColumn[Query::Column::GreaterOrEqual]->op =
+								Query::Column::Invalid;
+						opColumn[Query::Column::GreaterOrEqual] = NULL;
+					}
+				}
+
+				//Check c0><value1> && c0><value2>
+				if (opColumn[Query::Column::Greater] != NULL
+						&& opColumn[Query::Column::Greater]->op
+								!= Query::Column::Invalid
+						&& opColumn[Query::Column::Greater] != curQueryOp) {
+					//TODO: Keep the max
+					if (opColumn[Query::Column::Greater]->value
+							< curQueryOp->value) {
+						opColumn[Query::Column::Greater]->op =
+								Query::Column::Invalid;
+						opColumn[Query::Column::Greater] = curQueryOp;
+					} else {
+						curQueryOp->op = Query::Column::Invalid;
+						break;
+
+					}
+				}
+
+				break;
+//				////////////////////////////////////////////////////////////////////////////////////////
+			case Query::Column::GreaterOrEqual:
+#ifdef DEBUG
+				++GreaterOrEqual;
+#endif
+				//Check c0==value1> && c0>=<value2>, value1>value2
+				if (opColumn[Query::Column::Equal] != NULL
+						&& opColumn[Query::Column::Equal]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::Equal]->value
+							< curQueryOp->value) {
+						return false;
+					}
+
+				}
+				//Check c0<=<value1> && c0><value2>
+				if (opColumn[Query::Column::LessOrEqual] != NULL
+						&& opColumn[Query::Column::LessOrEqual]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::LessOrEqual]->value
+							< curQueryOp->value) {
+						return false;
+					} else {
+						//TODO: values the same replace them with (==)
+					}
+				}
+
+				//Check c0<<value1> && c0><value2>
+				if (opColumn[Query::Column::Less] != NULL
+						&& opColumn[Query::Column::Less]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::Less]->value
+							<= curQueryOp->value) {
+						return false;
+					}
+				}
+
+				//Check c0!=<value1> && c0>=<value2>, value1<value2
+				if (opColumn[Query::Column::NotEqual] != NULL
+						&& opColumn[Query::Column::NotEqual]->op
+								!= Query::Column::Invalid) {
+					if (opColumn[Query::Column::NotEqual]->value
+							< curQueryOp->value) {
+						//TODO: remove != from queries and (<=) -> (<)
+						opColumn[Query::Column::NotEqual]->op =
+								Query::Column::Invalid;
+						opColumn[Query::Column::NotEqual] = NULL;
+
+					}
+				}
+
+				//Check c0>=<value1> && c0><value2>
+				if (opColumn[Query::Column::GreaterOrEqual] != NULL
+						&& opColumn[Query::Column::GreaterOrEqual]->op
+								!= Query::Column::Invalid
+						&& opColumn[Query::Column::GreaterOrEqual]
+								!= curQueryOp) {
+					//TODO: Keep the max
+					if (opColumn[Query::Column::GreaterOrEqual]->value
+							< curQueryOp->value) {
+						opColumn[Query::Column::GreaterOrEqual]->op =
+								Query::Column::Invalid;
+						opColumn[Query::Column::GreaterOrEqual] = curQueryOp;
+
+					} else {
+						curQueryOp->op = Query::Column::Invalid;
+						break;
+					}
+
+				}
+
+				//Check c0><value1> && c0><value2>
+				if (opColumn[Query::Column::Greater] != NULL
+						&& opColumn[Query::Column::Greater]->op
+								!= Query::Column::Invalid) {
+//					//TODO: Keep the max
+					if (opColumn[Query::Column::Greater]->value
+							< curQueryOp->value) {
+						opColumn[Query::Column::Greater]->op =
+								Query::Column::Invalid;
+						opColumn[Query::Column::Greater] = NULL;
+					} else {
+						curQueryOp->op = Query::Column::Invalid;
+						break;
+					}
+				}
+
+				break;
+
+			case Query::Column::Invalid:
+				continue;
+			}
+
+		}
+	}
+	return true;
+
+}
+
+#endif /* VALIDATION_H_ */
