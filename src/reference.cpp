06f561bcf62d9063967e5756350e45686162eec6
costantinos <costa.costantinos@gmail.com>
2015-03-20 21:23:54 +0200

    fixing memory

--- src/reference.cpp
+++ src/reference.cpp
@@ -87,7 +87,7 @@ static unordered_map<uint32_t, uint64_t*> *relations;
 //GLOBAL visible to Validation.h
 std::array<uint64_t, MINMAX>***transactionStats;
 //--------------------------------------------------
-static stx::btree_map<uint64_t,unordered_map<uint32_t, stx::btree_map<uint64_t, set<uint64_t*>>>*> transactionHistory;
+static stx::btree_map<uint64_t,unordered_map<uint32_t, stx::btree_map<uint64_t, unordered_set<uint64_t*>>>*> transactionHistory;
 static btree::btree_map<uint64_t, bool> queryResults;
 
 #ifdef HISTOGRAM
@@ -157,8 +157,8 @@ static void processTransaction(const Transaction& t) {
 	}
 	cerr << "]" << endl;
 #endif
-	unordered_map<uint32_t, stx::btree_map<uint64_t, set<uint64_t*>>>* operations =
-			new unordered_map<uint32_t, stx::btree_map<uint64_t, set<uint64_t*>>>[relationCount];
+	unordered_map<uint32_t, stx::btree_map<uint64_t, unordered_set<uint64_t*>>>* operations =
+			new unordered_map<uint32_t, stx::btree_map<uint64_t, unordered_set<uint64_t*>>>[relationCount];
 	uint32_t index;
 	const TransactionOperationDelete* o;
 	const TransactionOperationInsert* ins;
@@ -261,13 +261,14 @@ static void processValidationQueries(const ValidationQueries& v) {
 	uint32_t pos = 0;
 	uint32_t index;
 	Query *q;
-	stx::btree_map<uint64_t, unordered_map<uint32_t, stx::btree_map<uint64_t, set<uint64_t*>>>*>::iterator iter;
+	stx::btree_map<uint64_t, unordered_map<uint32_t, stx::btree_map<uint64_t, unordered_set<uint64_t*>>>*>::iterator iter;
 	bool match;
 	Query::Column * c;
 	Query::Column * cLimit;
 	uint64_t tupleValue;
 	uint64_t queryValue;
 	bool result;
+	stx::btree_map<uint64_t, unordered_set<uint64_t*>>::iterator temp;
 
 	for (index = 0; index != v.queryCount; ++index) {
 		q = const_cast<Query*>(reinterpret_cast<const Query*>(reader));
@@ -329,8 +330,9 @@ static void processValidationQueries(const ValidationQueries& v) {
 
 			if(iter->second[q->relationId].size() == 0) continue;
 
-			std::set<uint64_t*> *tup = new std::set<uint64_t*>[q->columnCount];
-			std::set<uint64_t*> *intersection = tup;
+			//TODO: need only two - the intersected and the new one
+			std::set<uint64_t*> tup;
+			std::set<uint64_t*> *intersection = new std::set<uint64_t*>;
 
 			match = true;
 			c = q->columns;
@@ -339,13 +341,12 @@ static void processValidationQueries(const ValidationQueries& v) {
 			for (; c != cLimit; ++c, ++count) {
 				queryValue = c->value;
 				result = false;
-				stx::btree_map<uint64_t, set<uint64_t*>>::iterator temp;
-
+				tup.clear();
 				switch (c->op) {
 				case Query::Column::Equal: //0
 					if(iter->second[q->relationId][c->column].find(queryValue) != iter->second[q->relationId][c->column].end()){
 						result = true;
-						tup[count].insert(iter->second[q->relationId][c->column][queryValue].begin(),iter->second[q->relationId][c->column][queryValue].end());
+						tup.insert(iter->second[q->relationId][c->column][queryValue].begin(),iter->second[q->relationId][c->column][queryValue].end());
 					}
 					break;
 				case Query::Column::NotEqual: //1
@@ -353,7 +354,7 @@ static void processValidationQueries(const ValidationQueries& v) {
 						for(auto r = iter->second[q->relationId][c->column].begin(); r != iter->second[q->relationId][c->column].end(); ++r) {
 							if(r->first == queryValue) continue;
 							result = true;
-							tup[count].insert(r->second.begin(), r->second.end());
+							tup.insert(r->second.begin(), r->second.end());
 //						}
 					}
 					break;
@@ -363,7 +364,7 @@ static void processValidationQueries(const ValidationQueries& v) {
 						auto bound = iter->second[q->relationId][c->column].upper_bound(queryValue);
 						for(auto r = iter->second[q->relationId][c->column].begin(); r != bound; ++r) {
 							if(r->first < queryValue) {
-								tup[count].insert(r->second.begin(), r->second.end());
+								tup.insert(r->second.begin(), r->second.end());
 							}
 						}
 					}
@@ -374,7 +375,7 @@ static void processValidationQueries(const ValidationQueries& v) {
 						auto bound = iter->second[q->relationId][c->column].upper_bound(queryValue);
 						for(auto r = iter->second[q->relationId][c->column].begin(); r != bound; ++r) {
 							if(r->first <= queryValue) {
-								tup[count].insert(r->second.begin(), r->second.end());
+								tup.insert(r->second.begin(), r->second.end());
 							}
 						}
 					}
@@ -388,7 +389,7 @@ static void processValidationQueries(const ValidationQueries& v) {
 						if(temp != iter->second[q->relationId][c->column].end()) {
 							result = true;
 							for(; temp != iter->second[q->relationId][c->column].end(); ++temp) {
-								tup[count].insert(temp->second.begin(), temp->second.end());
+								tup.insert(temp->second.begin(), temp->second.end());
 							}
 						}
 					}
@@ -398,7 +399,7 @@ static void processValidationQueries(const ValidationQueries& v) {
 					if(temp != iter->second[q->relationId][c->column].end() && temp->first >= queryValue) {
 						result = true;
 						for(; temp != iter->second[q->relationId][c->column].end(); ++temp) {
-							tup[count].insert(temp->second.begin(), temp->second.end());
+							tup.insert(temp->second.begin(), temp->second.end());
 						}
 					}
 					break;
@@ -411,8 +412,8 @@ static void processValidationQueries(const ValidationQueries& v) {
 				}
 				else {
 					if(count > 0 && intersection->size() > 0) {
-						vector<uint64_t*> inter(intersection->size() + tup[count].size());
-						vector<uint64_t*>::iterator end = set_intersection(intersection->begin(), intersection->end(), tup[count].begin(), tup[count].end(), inter.begin());
+						vector<uint64_t*> inter(tup.size());
+						vector<uint64_t*>::iterator end = set_intersection(intersection->begin(), intersection->end(), tup.begin(), tup.end(), inter.begin());
 						inter.resize(end-inter.begin());
 
 						if(inter.size() == 0) {
@@ -423,10 +424,12 @@ static void processValidationQueries(const ValidationQueries& v) {
 						intersection->insert(inter.begin(), inter.end());
 					}
 					else {
-						intersection = tup+count;
+						intersection->insert(tup.begin(), tup.end());
+//						intersection = tup;
 					}
 				}
 			}
+			delete intersection;
 			if (match) {
 				conflict = true;
 				break;
